6ff54cd50c74cd52f2dd6c409c6733ad35978e98
sncktm <sncktm01@gmail.com>
2024-10-30 01:17:55 +0900

    ログイン機能のアクセス制限追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
+  before_action :forbid_login_user
   def top
-  end
-  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   post 'posts/:id/destroy' => "posts#destroy"
 
   get '/' => "home#top"
-  get 'about' => "home#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
