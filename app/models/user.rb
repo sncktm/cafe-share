b5fcfcdc639ffffa68bb4ac84ac3083666de58f1
sncktm <sncktm01@gmail.com>
2024-12-22 23:37:29 +0900

    emailバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, {presence: true}
-  validates :email, {presence: true, uniqueness: true}
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :prefecture_id, {presence: true}
 
   def posts
